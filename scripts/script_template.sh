d77b411f2222ae509178fb480083b64736920446
r02hw22 <r02hw22@maxlogin1.int.maxwell.abdn.ac.uk>
2026-04-25 18:47:56 +0100

    built gff3 lookup table

--- scripts/script_template.sh
+++ scripts/script_template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --mem 16G
+#SBATCH --mem 96G
 #SBATCH --partition uoa-compute
 #SBATCH -N 1
 #SBATCH -n 8
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=r02hw22@abdn.ac.uk
 #SBATCH --time=1-00:00:00
-#SBATCH --output=/uoa/home/r02hw22/sharedscratch/projecy_dark_genes/logs/outputs
+#SBATCH --output=/uoa/home/r02hw22/sharedscratch/project_dark_genes/logs/outputs/%x_%j.out
 #SBATCH --error=/uoa/home/r02hw22/sharedscratch/project_dark_genes/logs/errors/%x_%j.err
 
-# sbatch /uoa/home/r02hw22/sharedscratch/scripts/X.sh
+cd /uoa/home/r02hw22/sharedscratch/project_dark_genes/
